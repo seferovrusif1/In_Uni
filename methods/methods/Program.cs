df1156215fa9e3b6e1d5cc4e1fc2838ec291286f
seferovrusif1 <mi73sbd9a@code.edu.az>
2023-10-17 12:31:32 +0300

    asd

---
+++ methods/methods/Program.cs
@@ -0,0 +1,130 @@
+﻿namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array1 = { 2, 35, 34 };
+            //connect(array1, 1, 2, 3);
+            Console.WriteLine(Min(Array1));
+            Console.WriteLine(Hasil(Array1));
+            Console.WriteLine(Pow(3, 0));
+            Console.WriteLine(avg(Array1));
+        }
+
+
+        static void connect(int[] array1, params int[] array2)
+        {
+
+            int[] newarray = new int[array1.Length + array2.Length];
+
+            for (int i = 0; i < newarray.Length; i++)
+            {
+                if (i < array1.Length)
+                {
+                    newarray[i] = array1[i];
+                }
+                else
+                {
+                    newarray[i] = array2[i - array1.Length];
+                }
+            }
+            foreach (var num in newarray)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+
+
+        //static void connect(int[] array1, params int[] array2)
+        //{
+
+        //    int[] newarray = new int[array1.Length + array2.Length];
+        //    int i = 0;
+
+        //    for (; i < array1.Length; i++)
+        //    {
+        //        newarray[i] = array1[i];
+        //    }
+        //    for (; i > array1.Length && i < newarray.Length; i++)
+        //    {
+        //        newarray[i] = array2[i - array1.Length];
+
+        //    }
+        //    foreach (var num in newarray)
+        //    {
+        //        Console.WriteLine(num);
+        //    }
+
+        //}
+
+
+
+        static int Min(params int[] Array1)
+        {
+            int Min = Array1[0];
+            foreach (var Item in Array1)
+            {
+                if (Min > Item)
+                {
+                    Min = Item;
+                }
+            }
+            return Min;
+        }
+
+
+
+        static int Max(params int[] Array1)
+        {
+            int Max = Array1[0];
+            foreach (var Item in Array1)
+            {
+                if (Max < Item)
+                {
+                    Max = Item;
+                }
+            }
+            return Max;
+        }
+
+
+
+
+        static int Hasil(params int[] Array1)
+        {
+            return Max(Array1) * Min(Array1);
+        }
+
+
+
+
+        static int Pow(int n, int m)
+        {
+            int l = 1;
+            for (int i = 0; i < m; i++)
+            {
+                l = l * n;
+            }
+            return l;
+        }
+
+
+        static int avg(params int[] Array1)
+        {
+            int Sum = 0;
+            foreach (var item in Array1)
+            {
+                Sum += item;
+            }
+            return Sum / Array1.Length;
+        }
+
+    
+
+
+
+
+    }
+}
---
+++ methods_practice/methods_practice/Program.cs
@@ -0,0 +1,164 @@
+﻿namespace methods_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine("Tekdir yoxsa cut:    "+IsPair(4));
+            //Console.WriteLine("Sade vuruqlari: ");
+            //PrimeNum(12);
+            //Console.WriteLine(" 4 ve 5");
+            //Console.WriteLine(
+            //    "     Sum: "+ Sum(4,5)+ 
+            //    "     Difference: "+ Difference(4,5)+
+            //    "     Multiplication: "+ Multiplication(4,5)+ 
+            //    "     Division: "+ Division(4,5));
+
+            //for (; true;)                                     //yoxlamaq daha rahat olsun deyedi bu for, algoritme daxil deyil
+            //{
+            //    int x = Convert.ToInt32(Console.ReadLine());
+            //    int first = 0, second = 1, third = 0;        //fikrimzde 1-ci ve 2 ci elementleri tuturq sonrada onlari artirib deyismek ucun 3 u elementi istifade edirik 
+            //    Console.WriteLine(0);
+            //    for (; true;)
+            //    {
+            //        if (first < x)
+            //        {
+            //            Console.WriteLine(second);
+            //            third = first + second;
+            //            first = second;
+            //            second = third;
+            //        }
+            //        else
+            //        {
+            //            break;
+            //        }
+            //    }
+            //}
+
+
+            Multiplication(2, 3);
+            Multiplication(2, 3, 4);
+
+
+        }
+
+        static void Multiplication(int a,int b)
+        {
+            Console.WriteLine("Multiplication 2 num: "+a*b);
+        }
+
+        static void Multiplication(int a, int b,int c)
+        {
+            Console.WriteLine("Multiplication 3 num: " + a * b*c);
+        }
+
+
+
+
+
+        //TAsk1   cut yoxsa tex olmasina bax
+
+        static string IsPair(int n)
+        {
+            string result = "";
+            if (n % 2 == 0)
+            {
+                result = "Cut ededdir";
+            }
+            else
+            {
+                result = "Tek ededdir";
+            }
+            return result;
+        }
+
+        //TAsk2  sade vuruqlara ayir
+        static void PrimeNum(int n)
+        {
+            int i = 2;
+            for (; true;)
+            {
+
+                if (n == 1)
+                {
+                    break;
+                }
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                    n = n / i;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+
+
+        //static void PrimeNum111(int n)
+        //{
+        //    int i = 2;
+        //    int ii = 2;
+        //    for (; true;)
+        //    {
+        //        if (n == 1)
+        //        {
+        //            break;
+        //        }
+        //        if (n % i == 0)
+        //        {
+        //            if (ii != i)
+        //            {
+        //                Console.WriteLine(ii);
+
+        //            }
+        //            n = n / i;
+
+        //        }
+        //        else
+        //        {
+        //            ii = i;
+
+        //            i++;
+        //        }
+
+        //    }
+        //}
+
+
+        //Task 3 calc
+        static float Sum(float n, float m)
+        {
+            return n + m;
+        }
+        static float Difference(float n, float m)
+        {
+            return n - m;
+        }
+        static float Multiplication(float n, float m)
+        {
+            return n * m;
+        }
+        static float Division(float n, float m)
+        {
+            return n / m;
+        }
+
+
+
+    }
+}
