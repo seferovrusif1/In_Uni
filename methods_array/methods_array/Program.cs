6cb4bd8b79fed9fbae405e30cc7e74faffc9961a
seferovrusif1 <mi73sbd9a@code.edu.az>
2023-10-18 14:00:19 +0400

    dsjfh

---
+++ methods_array/methods_array/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace methods_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exchange(1,2,3,4);
+        }
+        
+        static void Exchange( params int[] array)
+        {
+            int maxI = Max(array);
+            int minI = Min(array);
+            int c = array[maxI];
+
+            array[maxI] = array[minI];
+            array[minI] = c;
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        static int Max(int[] array)
+        {
+            int max = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[max] < array[i])
+                {
+                    max = i;
+                }
+            }
+            return max;
+
+        }
+        static int Min(int[] array)
+        {
+            int min = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[min] > array[i])
+                {
+                    min = i;
+                }
+            }
+            return min;
+
+        }
+    }
+}
---
+++ methods_ref_out_referance_data/methods_ref_out_referance_data/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace methods_ref_out_referance_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[0];
+            for (;true;)
+            {
+                Console.WriteLine("Element elave etmek istiyrsizse y yaz:");
+                string y = Console.ReadLine();
+                if (y == "y")
+                {
+                    Array.Resize(ref arr, arr.Length + 1);
+                    Console.WriteLine("Elementi daxil edin:");
+                    arr[arr.Length - 1] = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    break;
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //int a = 8;
+            //int b = 9;
+            //Exchange(ref a, ref b);
+            //Console.WriteLine("a: "+ a+ "     b: "+b);
+
+        }
+
+
+
+        static void Exchange(ref int a, ref int b)
+        {
+            a = a + b;
+            b = a - b;
+            a = a - b;
+        }
+
+    }
+}
