e7ff5664d75373266554625017006757a952af8d
seferovrusif1 <mi73sbd9a@code.edu.az>
2023-10-19 13:25:24 +0400

    asd

---
+++ Class/Class/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car x001 = new Car("BMW", "M5","ah");
+            //Car x002 = new Car("BMW", "M3");
+            //x002.Color ="Red";
+            //x002.SeatNumber = 124;
+            //Car x003 = new Car("Mercedes", "e300");
+            //x002.Color = "Green";
+            //x002.SeatNumber = 125;
+            //Car x004 = new Car("BMW", "M4");
+            //x002.Color = "Red";
+            //x002.SeatNumber = 137;
+
+            //string[] arr =Car.;
+            //Console.WriteLine($"{x001.Brand} {x001.Model}");
+
+            Student x001 = new Student("Rusif", "Safarov");
+            Student x002 = new Student("ALi", "Safarov", "asfd");
+            Student x003 = new Student("Nihad", "Safarov", "asfd", 23);
+            Student x004 = new Student("Kerem", "Safarov", "asfd", 23, true);
+            Student x007 = new Student("Cefer", "Safarov", "asfd", 23, true);
+            Student x005 = new Student("Vuqar", "Safarov", "asfd", 23, true);
+            Console.WriteLine(x004.Name+x004.Surname+x004.Group);
+
+            Student[] arr = new Student[] {x001,x002,x003,x004,x005,x007} ;
+
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine($"{item.Name} {item.Surname} {item.Group} {item.Age} {item.HasScholarship}");
+            }
+
+
+
+        }
+    }
+    
+}
---
+++ Class/Class/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Age;
+        public bool HasScholarship; 
+        public Student(string Name, string Surname)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+        }
+        public Student(string Name, string Surname,string Group) : this(Name, Surname)
+        {
+            this.Group = Group;
+        }
+        public Student(string Name, string Surname, string Group, int Age) : this(Name, Surname,Group)
+        {
+            this.Age = Age;
+        }
+        public Student(string Name, string Surname, string Group, int Age, bool HasScholarship) : this(Name, Surname,Group,Age)
+        {
+            this.HasScholarship = HasScholarship;
+        }
+    }
+}
