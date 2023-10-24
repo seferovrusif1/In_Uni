97d9978644b69da0336c1858e80e49fe69d7adf0
seferovrusif1 <mi73sbd9a@code.edu.az>
2023-10-24 13:57:12 +0400

    sd

---
+++ ConsoleApp1/Collection/IntList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    internal class IntList { 
+        public int[] arr = new int[0];
+
+
+
+
+    public void Add(params int[] number)
+    {
+        foreach (var item in number)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = item;
+        }
+
+    }
+
+
+
+
+
+
+
+    public void Add(int number)
+    {
+        Array.Resize(ref arr, arr.Length + 1);
+        arr[arr.Length - 1] = number;
+
+    }
+    public void Get(int index)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i == index)
+            {
+                Console.WriteLine(arr[i]);
+
+            }
+        }
+    }
+    public int[] GetAll()
+    {
+        return arr;
+    }
+    public void Print()
+    {
+        foreach (var item in arr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ ConsoleApp1/Collection/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            IntList a = new IntList();
+
+            a.Add(3);
+            a.Add(1, 2, 3);
+            a.Get(1);
+            Console.WriteLine(a.GetAll());
+            a.Print();
+
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Add:-0\nadd params:1\nget index:2\nget all:3\nprint:4");
+            //    string b= Console.ReadLine();
+            //    switch (b)
+            //    {
+            //        case "0":
+
+            //            break;
+            //        case "1":
+            //            break;
+            //        case "2":
+            //            break;
+            //        case "3":
+            //            break;
+            //        case "4":
+            //            break;
+            //    }
+            //}
+        }
+    }
+}
