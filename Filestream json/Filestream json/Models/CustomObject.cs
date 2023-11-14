f56693d1eb06adeed2f6460f7b045e43d352d0ef
seferovrusif1 <mi73sbd9a@code.edu.az>
2023-11-14 13:08:31 +0400

    json

---
+++ Filestream json/Filestream json/Models/CustomObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filestream_json.Models
+{
+    internal class CustomObject
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+       
+    
+    }
+}
---
+++ Filestream json/Filestream json/Program.cs
@@ -0,0 +1,53 @@
+﻿using Filestream_json.Models;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text.Json.Serialization;
+
+namespace Filestream_json
+{
+    internal class Program
+    {
+        //public static string Root = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\"));
+        public static string Root = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\"));
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Root);
+            //C: \Users\Birinci novbe\Desktop\New folder\Filestream json\Filestream json\NewFolder\
+            if (!Directory.Exists(Path.Combine(Root, "Models")))
+            {
+                Directory.CreateDirectory(Path.Combine(Root, "Models"));
+            }
+            if (!Directory.Exists(Path.Combine(Root, "Data")))
+            {
+                Directory.CreateDirectory(Path.Combine(Root, "Data"));
+            }
+            if (!File.Exists(Path.Combine(Root, "Data", "jsonData.json")))
+            {
+                File.Create(Path.Combine(Root, "Data", "jsonData.json"));
+            }
+
+
+
+
+
+            HttpClient client = new HttpClient();
+            
+            string data = client.GetStringAsync("https://jsonplaceholder.typicode.com/posts").Result;
+            var a= JsonConvert.DeserializeObject<List<CustomObject>>(data);
+            
+            string strData = JsonConvert.SerializeObject(a);
+            StreamWriter sw = new StreamWriter(Path.Combine(Root, "Data", "jsonData.json"));
+            sw.WriteLine(strData);
+            sw.Close();
+            // StreamReader sr = new StreamReader(Path.Combine(Root, "Data", "jsonData.json"));
+            // Console.WriteLine(sr.ReadToEnd());
+
+
+            //using (StreamWriter sw = new StreamWriter(Path.Combine(Root, "Data", "jsonData.json")))
+            //{
+            //    sw.WriteLine(strData);
+            //}
+        }
+
+    }
+}
